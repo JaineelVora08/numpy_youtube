d9b90f408a3e0c5c4ea9fd3ab11df05a2a338d44
JaineelVora08 <jaineelvora2006@gmail.com>
2024-12-10 22:16:53 +0530

    Update and rename intro.py to class1.py

--- class1.py
+++ class1.py
@@ -7,13 +7,18 @@ list1 = [1,2,3,4,5]
 list2 = ["John Elder", 41, list1, True]
 #print(list2)
 
-
-# Numpy - Numeric Python
+# Numpy - Nu meric Python
 # ndarray = n-dimensional array
 
 np1 = np.array([0,1,2,3,4,5,6,7,8,9])
 print(np1)
-print(np1.shape)
+print(np1.shape) 
+# np.shape gives dimensions of array and np.size gives size of array and np.itemsize gives size of items/elements in array
+# array.size and np.size(array) are same
+# arr = np.array([[1, 2, 3], [4, 5, 6]], dtype=np.int32)
+# arr.shape  # Output: (2, 3)
+# arr.size  # Output: 6 (NOT 2) - it multiplies the dimensions in shape
+# len(arr)  # Output: 2 - returns the size of the first dimension/axis of the array (the number of rows in a 2D array).
 
 # Range
 np2 = np.arange(10)
