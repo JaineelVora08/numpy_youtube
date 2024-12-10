5df0c4fac21c0288291136dbee3635d360d265cd
JaineelVora08 <jaineelvora2006@gmail.com>
2024-12-10 22:07:58 +0530

    Update copyview.py

--- copyview.py
+++ copyview.py
@@ -1,10 +1,14 @@
 import numpy as np
 
 # Copy Vs. View
+# copy is normal copy, view me view ya org dono me se kisi me bhi change kiya to dono me changes dikhenge
 np1 = np.array([0,1,2,3,4,5])
 
 # Create a view
 np2 = np1.view()
+
+# you can directly print lists/arrays in python
+# list can have different data types
 
 print(f'Original NP1 {np1}')
 print(f'Original NP2 {np2}')
