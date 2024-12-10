38a7ef852a4b9a336fc5c8ca5938ac8aeef0450f
JaineelVora08 <jaineelvora2006@gmail.com>
2024-12-10 23:01:11 +0530

    Update and rename copyview.py to class4.py

--- class4.py
+++ class4.py
@@ -7,6 +7,19 @@ np1 = np.array([0,1,2,3,4,5])
 # Create a view
 np2 = np1.view()
 
+# SLICING ARRAY ALSO CREATES VIEW
+# rr = np.array([1, 2, 3, 4, 5])
+# view_arr = arr.view()
+# slice_arr = arr[1:4]
+
+# view_arr[0] = 99  # Modify the view
+# slice_arr[0] = 88  # Modify the slice
+# isse jo org array me element tha vo bhi change hoga
+
+# print("Original:", arr)  # Output: [99 88 3 4 5]
+# print("View:", view_arr)  # Output: [99 88 3 4 5]
+# print("Slice:", slice_arr)  # Output: [88 3 4]
+
 # you can directly print lists/arrays in python
 # list can have different data types
 
@@ -30,3 +43,7 @@ np2[0] = 41
 print(f'Changed NP1 {np1}')
 print(f'Original NP2 {np2}')
 '''
+
+#To check
+If array.base is None, the array is a standalone object (a copy).
+If array.base is not None, the array is a view, and array.base points to the original data it references.
