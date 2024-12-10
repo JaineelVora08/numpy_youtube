a79a0d1bca69e97c4d79b070d4450e05e1eaa9ea
JaineelVora08 <jaineelvora2006@gmail.com>
2024-12-10 23:05:04 +0530

    Update and rename shaper.py to class5.py

--- class5.py
+++ class5.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+# reshape se view banega NOT copy
+
 # Create 1-D Numpy Array and Get Shape
 np1 = np.array([1,2,3,4,5,6,7,8,9,10,11,12])
 print(np1)
@@ -9,7 +11,7 @@ print(np1.shape)
 #np2 = np.array([[1,2,3,4,5,6],[7,8,9,10,11,12]])
 #print(np2)
 #print(np2.shape)
-
+ 
 # Reshape 2-D
 #np3 = np1.reshape(3,4)
 #print(np3)
@@ -21,6 +23,8 @@ print(np4)
 print(np4.shape)
 
 # Flatten to 1-D
+# always Reshapes the array np4 into a 1D array (flat array) 
+# The -1 tells NumPy to infer the size of the new dimension automatically based on the original array's size.
 np5 = np4.reshape(-1)
 print(np5)
 print(np5.shape)
