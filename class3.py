c614cb68f3431b24445c420f6239cc4047c55b0e
JaineelVora08 <jaineelvora2006@gmail.com>
2024-12-10 22:53:24 +0530

    Update and rename fun.py to class3.py

--- class3.py
+++ class3.py
@@ -6,19 +6,21 @@ print(np1)
 
 # Square root of each element 
 #print(np.sqrt(np1))
+# 4 ka 4.    aise print
 
 # Absolute Value
 #print(np.absolute(np1))
 
-# Exponents
+# Exponents - e^number
 #print(np.exp(np1))
 
 # Min/Max
 #print(np.max(np1))
 #print(np.min(np1))
 
-# Sign positive or negative
+# Sign positive or negative - returns -1 or 0
 #print(np.sign(np1))
 
 # Trig sin cos log
 print(np.log(np1))
+#-ve ke liye nan
