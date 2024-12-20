e001f9ee55f9f5059ac2b0f51344c4660174eab6
JaineelVora08 <jaineelvora2006@gmail.com>
2024-12-20 12:41:53 +0530

    Update class3_functions.py

--- class3_functions.py
+++ class3_functions.py
@@ -24,3 +24,24 @@ print(np1)
 # Trig sin cos log
 print(np.log(np1))
 #-ve ke liye nan
+
+arr.sum(axis=0)
+# along rows jaayega bcs axis=0, so vo har ek column ka sum dega
+
+# arr.flat in NumPy is a flat iterator over a multi-dimensional array, which allows you to iterate through the array as if it were a 1D array, without actually flattening it into a new array.
+for item in ar.flat:
+  print item
+
+# find the index of the maximum value in an array. 
+# agar 2D pe kiya to vo usko flattened array ki tarah dekhke index return karega
+arr.argmax()
+
+arr.argmax(axis=0)
+# isse vo har column ke max index ko return karega
+
+# The arr.argsort() function in NumPy is used to return the indices that would sort an array.
+arr.argsort(axis=-1, kind='quicksort', order=None)
+# The axis along which to sort. The default value is -1, which means it will sort the flattened array (1D).
+
+# arr1+arr2 se normal matrix addition - if numpy arrays
+# but list1+list2 se list extend ho jaayegi
